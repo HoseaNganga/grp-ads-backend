35fe3d694ba5ece7704c5738cd4b76942218ed88
Hosea Kibe Nganga <hoseanganga27@gmail.com>
2025-07-13 02:48:08 +0300

    add email verification using 6-digit code and app password SMTP setup

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller";
+import {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  deleteUserById,
+  verifyUserByCode,
+} from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/users", getAllUsers);
+router.delete("/users/:id", deleteUserById);
+router.post("/verify-code", verifyUserByCode);
 
 export default router;
